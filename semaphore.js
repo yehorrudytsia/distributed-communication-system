ca5f2bb9e48b38a3968c87b13d8d93ea42921f4d
Yegor Ruditsa <egorrudica@gmail.com>
2020-10-23 14:13:09 +0300

    feat: change semaphore.js

--- semaphore.js
+++ semaphore.js
@@ -7,6 +7,24 @@ class Semaphore {
     this.size = size;
     this.queue = [];
   }
+
+  enter() {
+    return new Promise((resolve, reject) => {
+      if (this.counter > 0) {
+        this.counter--;
+        resolve();
+        return;
+      }
+      if (this.queue.length >= this.size) {
+        reject(new Error('Semaphore queue is full'));
+        return;
+      }
+      const timer = setTimeout(() => {
+        reject(new Error('Semaphore timeout'));
+      }, this.timeout);
+      this.queue.push({ resolve, timer });
+    });
+  }
 }
 
 module.exports = Semaphore;
