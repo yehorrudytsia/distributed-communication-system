eab6cd878992d86fdaf7bd2b8db1067e149cee66
yegorrudica <yehorrudytsia@gmail.com>
2020-11-06 13:23:36 +0200

    Add project structure

---
+++ storage/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SystemUsers (Login, Password, FullName)
+VALUES ('jacquefresco', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$XWzCkBbnH/pAxX24vPbeyhIFXJI4EDeqrD/2TVKthmQ$unx3RSASN2pIjGE5Ij/AHbQr7iVja7//gEvH9PJqQtmi04ztGOaDozOnCPajMnwoiNwh7zQUf29gj2QHNK0CLQ', 'Jacque Fresco');
+
+-- jacquefresco/fresco
