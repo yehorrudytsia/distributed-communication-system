26658518fbf72be618c7e4d6b2db9eebd9e8c215
yegorrudica <yehorrudytsia@gmail.com>
2020-11-20 15:18:04 +0200

    2

--- tests/unit.database.js
+++ tests/unit.database.js
@@ -20,6 +20,43 @@ const PATH = process.cwd();
   setTimeout(async () => {
     const databaseConfig = config.units.database;
     const database = new Database(databaseConfig);
+    const empty = 'empty';
+    try {
+      const user = { login: empty, password: empty, fullName: empty };
+      const result = await database.insert('SystemUsers', user);
+      assert(result);
+      assert.equal(result.rowCount, 1);
+    } catch (err) {
+      console.log(err.stack);
+      process.exit(1);
+    }
+    try {
+      const fields = ['login', 'password'];
+      const where = { login: empty };
+      const [record] = await database.select('SystemUsers', fields, where);
+      assert.equal(record.login, empty);
+      assert.equal(record.password, empty);
+    } catch (err) {
+      console.log(err.stack);
+      process.exit(1);
+    }
+    try {
+      const delta = { password: empty };
+      const where = { login: empty };
+      const result = await database.update('SystemUsers', delta, where);
+      assert.equal(result.rowCount, 1);
+    } catch (err) {
+      console.log(err.stack);
+      process.exit(1);
+    }
+    try {
+      const where = { login: empty };
+      const result = await database.delete('SystemUsers', where);
+      assert.equal(result.rowCount, 1);
+    } catch (err) {
+      console.log(err.stack);
+      process.exit(1);
+    }
     database.close();
   }, 100)
 })();
