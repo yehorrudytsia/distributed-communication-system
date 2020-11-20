ba641858ad281e534619bf54ecfdbfa7e37240cb
yegorrudica <yehorrudytsia@gmail.com>
2020-11-20 15:00:52 +0200

    1

--- config/monitoring.js
+++ config/monitoring.js
@@ -1,3 +1,3 @@
 module.exports = {
-  interval: 10000,
+  interval: 60000,
 };
---
+++ static/pages/login.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const BASE = '/api/';
+
+const loadMethods = methods => {
+  const api = {};
+  for (const method of methods)
+  {
+  api[method] = (args = {}) =>  new Promise((resolve, reject) => {
+    const url = BASE + method;
+    console.log(url, args);
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(args),
+    }).then(res => {
+      const { status } = res;
+      if (status !== 200) {
+        reject(new Error(`Status Code: ${status}`));
+        return;
+      }
+      resolve(res.json());
+      });
+    });
+  }
+  return api;
+};
+
+const api = loadMethods([
+  'getUser',
+]);
+
+const login = async () => {
+
+  let user = {
+                login : document.getElementById("logLoginInput").value,
+            };
+
+  console.dir(user);
+  const id = await api.getUser(user)
+      .catch(err => (err))
+      .then(setTimeout(() => {
+        document.getElementById('output')
+                .innerHTML = 'Invalid login or password!'
+      }, 500));
+
+  document
+      .getElementById('output')
+      .innerHTML = 'Success!'
+}
+login();
